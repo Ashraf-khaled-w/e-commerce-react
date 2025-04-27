1f555be4b881f074ac4f44a6e91e9a96fd04e2d8
Ashraf <ashraf.khaled.w@gmail.com>
2025-04-27 15:59:10 +0300

    Configure router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
-import Home from './components/Home/Home'
-import ProductPage from './components/ProductPage/ProductPage'
-import About from './components/About/About'
-import Auth from './components/Auth/Auth'
-
-
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import ProductPage from "./components/ProductPage/ProductPage";
+import About from "./components/About/About";
+import Auth from "./components/Auth/Auth";
 
 function App() {
+  let routers = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          { index: true, element: <Home /> },
+          { path: "productPage", element: <ProductPage /> },
+          { path: "about", element: <About /> },
+        ],
+      },
+      { path: "auth", element: <Auth /> },
+    ],
+    { basename: "/e-commerce-react/" }
+  );
 
-  let routers = createBrowserRouter([
-    {path:'/',element:<Layout/>,children:[
-      {index:true,element:<Home/>},
-      {path:'productPage',element:<ProductPage/>},
-      {path:'about', element:<About/>}
-    ]},
-    {path:'auth',element:<Auth/>},
-    
-  ])
-
-  
-  
-
-  return <>
-  <RouterProvider router={routers}></RouterProvider>
-  </>
-    
-  
+  return (
+    <>
+      <RouterProvider router={routers}></RouterProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
